079b9c58bcc02e349a26d046e29308918ee1363d
Mayank Jha <mayank25080562@gmail.com>
2014-03-10 18:59:51 +0530

    Added libre license.

--- fc/FontCompare.py
+++ fc/FontCompare.py
@@ -1,4 +1,36 @@
 """
+    Copyright (C) 2013 Mayank Jha <mayank25080562@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
+    Copyright (C) 2013 Mayank Jha <mayank25080562@gmail.com>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+"""
 Contains methods for making comparisions using a host of tests.
 They are also for modifying a glyph by altering its
 serif, stroke , stem thickness, size, italic angle etc.
